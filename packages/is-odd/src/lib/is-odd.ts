a3458e27b9cfabd130ab87b331eed6e27ac3b5de
019426 <lijie019426@htsc.com>
2023-06-27 15:03:06 +0800

    feat: 修改is-odd

--- packages/is-odd/src/lib/is-odd.ts
+++ packages/is-odd/src/lib/is-odd.ts
@@ -1,7 +1,5 @@
 import { isEven } from '@myorg/is-even';
 
-export function isOdd(x: number): boolean {
-  console.log(x);
-  
+export function isOdd(x: number): boolean {  
   return !isEven(x);
 }
